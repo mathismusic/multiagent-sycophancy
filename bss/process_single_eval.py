77ec641ae997ded97625fd5ca480d9e1ff95619f
aalrabah <alrabah2@illinois.edu>
2026-01-03 15:49:11 +0300

    updated eval files

--- bss/process_single_eval.py
+++ bss/process_single_eval.py
@@ -1,6 +1,6 @@
 from process_eval_results import overall_from_csv
 
-csv_path = "eval_results/mmlu_debate_metadata_iterated__EVAL/summary_all.csv"
+csv_path = "/Users/abdulrahmanalrabah/Desktop/UIUC/Fall 2025/CS 546/Project/project/multiagent-sycophancy/bss/mmlu_debate_metadataـ__EVAL/summary_all.csv"
 title = "MMLU Debate Results (Iterated)"
 df = overall_from_csv(csv_path, title)
 print(df.to_string(index=False))
