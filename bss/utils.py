0c1daa024bb79f475680b084f861c81b0eda6810
Ritwik Garg <gargritwik08@gmail.com>
2026-01-04 00:32:41 -0600

    reordered model order in heatmap

--- bss/utils.py
+++ bss/utils.py
@@ -29,11 +29,11 @@ load_dotenv()
 
 models_map = {
     # "llama1b": "meta-llama/Llama-3.2-1B-Instruct",
-    # "llama3b": "meta-llama/Llama-3.2-3B-Instruct",
-    # "llama8b": "meta-llama/Llama-3.1-8B-Instruct",
-    # "qwen3b": "Qwen/Qwen2.5-3B-Instruct",
-    # "qwen7b": "Qwen/Qwen2.5-7B-Instruct",
-    # "qwen14b": "Qwen/Qwen2.5-14B-Instruct",
+    "llama3b": "meta-llama/Llama-3.2-3B-Instruct",
+    "llama8b": "meta-llama/Llama-3.1-8B-Instruct",
+    "qwen3b": "Qwen/Qwen2.5-3B-Instruct",
+    "qwen7b": "Qwen/Qwen2.5-7B-Instruct",
+    "qwen14b": "Qwen/Qwen2.5-14B-Instruct",
     "qwen32b": "Qwen/Qwen2.5-32B-Instruct",
     # "mistral3b": "mistralai/Ministral-3-3B-Instruct-2512",
     # "mistral8b": "mistralai/Ministral-3-8B-Instruct-2512",
